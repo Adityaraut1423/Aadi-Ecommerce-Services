89c30a4ca9d73bbe51d722d190507500390d114f
Aditya <emailforproject659@gmail.com>
2026-07-23 01:17:31 +0530

    Add HomeController root endpoint for health checks

---
+++ src/main/java/com/aadiandjava/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.aadiandjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Aadi Store E-Commerce API is live and operational!";
+    }
+}
